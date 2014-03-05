1f95d3269195544aec52f67ee365a8ccc1b1951d
mdejean <mrd79@case.edu>
2014-03-05 09:20:52 -0500

    encode implementation - use uint8_t

--- src/encode.c
+++ src/encode.c
@@ -1,33 +1,51 @@
 #include <stdlib.h> //for memcpy
 #include "encode.h"
 
+bool encode_init(size_t frame_len,  
+    const uint8_t* start_framing;
+    size_t start_framing_len,
+    const uint8_t* end_framing,
+    size_t end_framing_len) {
+  s->frame_len         = frame_len;
+  s->start_framing     = start_framing; 
+  s->start_framing_len = start_framing_len;   
+  s->end_framing       = end_framing; 
+  s->end_framing_len   = end_framing_len;  
+}
+
 size_t frame_message(
     encode_state* s, 
-    const char* message, 
+    const uint8_t* message, 
     size_t message_len, 
-    char* data, 
+    uint8_t* data, 
     size_t data_len) {
   //TODO: handle data_len being too small by sending only some of the message
   if (data_len < message_len + (message_len/s->frame_len + 1) * 
       (s->start_framing_len + s->end_framing_len)) {
     return 0;
   }
-  char* data_moving = data;
+  uint8_t* data_moving = data;
   for (size_t i = 0; i < message_len; i += s->frame_len) {
-    memcpy(data_moving, s->start_framing, start_framing_len);
-    data_moving += s->start_framing_len;
-    memcpy(data, &message[i], 
+    if (s->start_framing_len > 0) {
+      memcpy(data_moving, s->start_framing, s->start_framing_len);
+      data_moving += s->start_framing_len;
+    }
+    
+    memcpy(data_moving, &message[i], 
       ((message_len - i) < s->frame_len) ? message_len - i : s->frame_len);
     data_moving += ((message_len - i) < s->frame_len) ? message_len - i : s->frame_len; //FIXME
-    memcpy(data, s->start_framing, end_framing_len);
-    data_moving += s->end_framing_len;
+    
+    if (s->end_framing_len > 0) {
+      memcpy(data_moving, s->end_framing, s->end_framing_len);
+      data_moving += s->end_framing_len;
+    }
   }
   return data_moving - data;
 }
 
 size_t encode_message(
     encode_state* s,
-    const char* data, 
+    const uint8_t* data, 
     size_t data_len, 
     sample_t* symbols, 
     size_t symbols_len) {
@@ -36,10 +54,10 @@ size_t encode_message(
   }
   for (size_t i = 0; i < data_len; i++) {
     //it would be better to gray-code these, but we don't for compatibility
-    symbols[4*i + 0] = (data[i] & 0x03);
-    symbols[4*i + 1] = (data[i] & 0x0C) >> 2;
-    symbols[4*i + 2] = (data[i] & 0x30) >> 4;
-    symbols[4*i + 3] = (data[i] & 0xC0) >> 6;
+    symbols[4*i + 0] = -SAMPLE_MAX + (SAMPLE_MAX/3) * 2 * (data[i] & 0x03);
+    symbols[4*i + 1] = -SAMPLE_MAX + (SAMPLE_MAX/3) * 2 * ((data[i] & 0x0C) >> 2);
+    symbols[4*i + 2] = -SAMPLE_MAX + (SAMPLE_MAX/3) * 2 * ((data[i] & 0x30) >> 4);
+    symbols[4*i + 3] = -SAMPLE_MAX + (SAMPLE_MAX/3) * 2 * ((data[i] & 0xC0) >> 6);
   }
   return 4*data_len;
 }
--- src/encode.h
+++ src/encode.h
@@ -5,11 +5,17 @@
 
 typedef struct encode_state_ {
   size_t frame_len; //number of message *bytes* in each frame
-  const char* start_framing;
+  const uint8_t* start_framing;
   size_t start_framing_len;
-  const char* end_framing;
+  const uint8_t* end_framing;
   size_t end_framing_len;
 } encode_state;
+
+bool encode_init(size_t frame_len,  
+    const uint8_t* start_framing;
+    size_t start_framing_len,
+    const uint8_t* end_framing,
+    size_t end_framing_len);
 
 /*TODO: consider messages that are too long to be stored in the provided buffer
 - should some indicator of the number of message bytes consumed be provided? */
@@ -19,9 +25,9 @@ returns: amount of data used
 */
 size_t frame_message(
     encode_state* s, 
-    const char* message, 
+    const uint8_t* message, 
     size_t message_len, 
-    char* data, 
+    uint8_t* data, 
     size_t data_len);
 
 /* encode_data - turn framed message into PAM symbols 
@@ -29,7 +35,7 @@ returns: amount of symbols used
 letters2pam*/
 size_t encode_data(
     encode_state* s, 
-    const char* data, 
+    const uint8_t* data, 
     size_t data_len, 
     sample_t* symbols, 
     size_t symbols_len);
